ace1bc4d2f9332c0d486d36f71a0a44dd46bde0b
h0cean <ho3yaghi@gmail.com>
2023-10-10 23:07:18 +0330

    Update tailwind.config.js
    last one

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
+  safelist: [
+    {
+      pattern: /bg-(sky|yellow|green|red|purple|pink)-(400|500|600)/, // You can display all the colors that you need
+    },
+  ],
   theme: {
     fontFamily: {
       sans: ['iransans'],
@@ -8,39 +13,6 @@ export default {
 
     extend: {
       colors: {
-        // bg: 'var(--tg-theme-bg-color)',
-        // button: 'var(--tg-theme-button-color)',
-        // 'button-text': 'var(--tg-theme-button-text-color)',
-        // hint: 'var(--tg-theme-hint-color)',
-        // link: 'var(--tg-theme-link-color)',
-        // 'secondary-bg': 'var(--tg-theme-secondary-bg-color)',
-        // text: 'var(--tg-theme-text-color)',
-
-        // 'bg-body': 'var(--color-bg-body)',
-        // 'bg-primary': 'var(--color-bg-primary)',
-        // 'bg-secondary': 'var(--color-bg-secondary)',
-        // 'bg-darker': 'var(--color-bg-darker)',
-        // 'bg-dark-input': 'var(--color-bg-dark-input)',
-        // 'bg-gradiant-from': 'var(--color-bg-gradiant-from)',
-        // 'bg-gradiant-to': 'var(--color-bg-gradiant-to)',
-        // 'bg-board': 'var(--color-bg-board)',
-
-        // 'text-primary': 'var(--color-text-primary)',
-        // 'text-secondary': 'var(--color-text-secondary)',
-        // 'text-darker': 'var(--color-text-darker)',
-        // 'text-title': 'var(--color-text-title)',
-        // 'text-disabled': 'var(--color-text-disabled)',
-        // 'text-green': 'var(--color-text-green)',
-        // 'text-red': 'var(--color-text-red)',
-        // 'text-light': 'var(--color-text-light)',
-        // 'text-lighter': 'var(--color-text-lighter)',
-
-        // perfect: 'var(--color-perfect)',
-        // badge: 'var(--color-badge)',
-
-        // 'border-light': 'var(--color-border-light)',
-        // 'border-dark': 'var(--color-border-dark)',
-
         'bg-primary': 'var(--color-bg-primary)',
         'bg-secondary': 'var(--color-bg-secondary)',
         'bg-header': 'var(--color-bg-header)',
@@ -96,9 +68,6 @@ export default {
         sidebar: 'sidebar 0.2s ease-in',
         dropBG: 'dropBG 1s infinite linear',
         dropPiece: 'dropPiece var(--drop-piece-duration) ease-in',
-        highLowNextCardRight: 'highLowNextCardRight 0.4s linear',
-        highLowNextCardLeft: 'highLowNextCardLeft 0.4s linear',
-        highLowFlipCard: 'highLowFlipCard 0.5s linear',
         fade: 'fade 0.5s ease-in',
         overlay: 'overlay 0.2s linear',
       },
@@ -111,10 +80,6 @@ export default {
             transform: 'translateX(0)',
           },
         },
-        // dropBG: {
-        //   '0%': { 'background-position-y': 0 },
-        //   '100%': { 'background-position-y': 'calc(var(--size-cell) * 25 / 72)' },
-        // },
         dropBG: {
           '0%': { transform: 'translateY(0)' },
           '100%': { transform: 'translateY(calc(var(--size-cell) * 25 / 72))' },
@@ -127,46 +92,6 @@ export default {
           '100%': {
             transform:
               'translate(calc(var(--size-cell)*var(--col-index)*-1), calc(var(--size-cell)*var(--row-index)*-1))',
-          },
-        },
-        highLowNextCardRight: {
-          '0%': {
-            right: '50%',
-            transform: 'translateX(50%)',
-            top: '25%',
-            width: '8rem',
-            height: '12rem',
-          },
-          '100%': {
-            right: '0',
-            transform: 'translateX(0)',
-            top: '0',
-            width: '2.5rem',
-            height: '3.5rem',
-          },
-        },
-        highLowNextCardLeft: {
-          '0%': {
-            left: '50%',
-            transform: 'translateX(-50%)',
-            top: '25%',
-            width: '8rem',
-            height: '12rem',
-          },
-          '100%': {
-            left: '0',
-            transform: 'translateX(0)',
-            top: '0',
-            width: '2.5rem',
-            height: '3.5rem',
-          },
-        },
-        highLowFlipCard: {
-          '0%': {
-            transform: 'rotateY(180deg)',
-          },
-          '100%': {
-            transform: 'rotateY(0)',
           },
         },
         fade: {
